af850ebfd06002d0a837090555fa751145585bcf
Nazar Grycshuk <gryshchuk.nazar@gmail.com>
2024-03-15 20:06:31 +0100

    fix: commit

--- api/server.py
+++ api/server.py
@@ -6,14 +6,14 @@ from authlib.integrations.starlette_client import OAuth
 from starlette.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
 
-from utils.auth_helper import get_twitter_token, get_twitter_authorize_url_and_verifier
+from utils.auth_helper import get_twitter_token, get_twitter_authorize_url_and_verifier, client_id, client_secret
 
 from fastapi import FastAPI, Request
 import httpx
 
 app = FastAPI()
 
-
+redirect_uri = "https://gokind.xyz"
 
 @app.exception_handler(Exception)
 async def error_handler(request: Request, exc: Exception):
@@ -29,6 +29,7 @@ async def error_handler(request: Request, exc: Exception):
 def read_root():
     return FileResponse('index.html')
 
+#https://stackoverflow.com/questions/74649514/how-to-recreate-tweepy-oauth2userhandler-across-web-requests
 # Flask route to redirect user to Twitter's authorization page
 @app.get("/login/twitter")
 async def login_twitter():
@@ -37,7 +38,8 @@ async def login_twitter():
     #auth_url = f'''https://twitter.com/i/oauth2/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope=tweet.read%20users.read%20follows.read%20follows.write&state=state&code_challenge=challenge&code_challenge_method=plain'''
     #https://twitter.com/i/oauth2/authorize?response_type=code&client_id=N1gyR29FUXZuWi1UbGF4UTFIdmo6MTpjaQ&redirect_uri=https%3A%2F%2Fgokind.xyz%2Fauthorize%2Ftwitter&scope=tweet.read%22%2C%22users.read&state=Ep9dePp0xb6i5XlAjkfXj3wtvNdM8s&code_challenge=uoiBXrbwIGCCSwHKs1HPXi_h8MLLE4YGsdBYDcbHNnA&code_challenge_method=S256
     authorize_url, code_verifier = get_twitter_authorize_url_and_verifier("https://gokind.xyz/authorize/twitter")
-    print(authorize_url)
+    print(authorize_url, code_verifier)
+    #return JSONResponse(content={"authorize_url":authorize_url, "code_verifier":code_verifier }) 
     return RedirectResponse(url=authorize_url)
 
 
@@ -50,6 +52,47 @@ async def authorize_twitter(state, code, request: Request):
                       current_url=current_url,
                       twitter_verifier=twitter_verifier)
     return result
+
+import requests
+import base64
+
+
+@app.get("/v2/authorize/twitter")
+async def authorize_twitter(state, code, request: Request):
+    auth_code = code #"auth_code_from_callback"
+    code_verifier = "123" #"code_challenge_string"  # Should match the verifier used to generate the challenge
+    
+    
+# Encode the Client ID and Secret
+    client_credentials = f"{client_id}:{client_secret}"
+    client_credentials_bytes = client_credentials.encode("ascii")
+    base64_encoded_credentials = base64.b64encode(client_credentials_bytes)
+    authorization_header_value = base64_encoded_credentials.decode("ascii")
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Basic {authorization_header_value}"
+    }
+
+    token_url = "https://api.twitter.com/2/oauth2/token"
+    data = {
+        "code": auth_code,
+        "grant_type": "authorization_code",
+        "client_id": client_id,
+        "redirect_uri": redirect_uri,
+        "code_verifier": code_verifier
+    }
+
+    response = requests.post(token_url, data=data, headers=headers)
+
+    # Check the response
+    if response.ok:
+        tokens = response.json()
+        access_token = tokens.get("access_token")
+        refresh_token = tokens.get("refresh_token")  # If you requested offline_access
+        print("Access Token:", access_token)
+    else:
+        print("Failed to obtain tokens", response.text)
 
 
 
--- api/utils/auth_helper.py
+++ api/utils/auth_helper.py
@@ -2,6 +2,7 @@ import tweepy
 from oauthlib.oauth2 import OAuth2Error
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
+from typing import List
 
 client_id="N1gyR29FUXZuWi1UbGF4UTFIdmo6MTpjaQ"
 client_secret="JMLkZIaNg-IcmlrxzYucfxHzjfGS1En4oe1j2-HCTTfD8-4ypC"
@@ -10,7 +11,7 @@ code_verifier= "123"
 
 class NotTweepyOAuth2UserHandler(OAuth2Session):
     def __init__(
-        self, client_id: str, client_secret: str, redirect_uri: str, scope: list[str], code_verifier: str | None = None
+        self, client_id: str, client_secret: str, redirect_uri: str, scope, code_verifier: str
     ):
         super().__init__(client_id, redirect_uri=redirect_uri, scope=scope)
         self.auth = HTTPBasicAuth(client_id, client_secret)
@@ -33,7 +34,7 @@ class NotTweepyOAuth2UserHandler(OAuth2Session):
             code_verifier=self.code_verifier,
         )
 
-def _oauth2_handler(callback_url: str, code_verifier: str | None):
+def _oauth2_handler(callback_url: str, code_verifier: str ):
     return NotTweepyOAuth2UserHandler(
         client_id,
         client_secret,
@@ -42,13 +43,13 @@ def _oauth2_handler(callback_url: str, code_verifier: str | None):
         code_verifier=code_verifier,
     )
 
-def get_twitter_authorize_url_and_verifier(callback_url: str) -> tuple[str, str]:
+def get_twitter_authorize_url_and_verifier(callback_url: str):
     handler = _oauth2_handler(callback_url, code_verifier)
     authorize_url = handler.get_authorization_url()
     # the caller can now store code_verifier somehow
     return authorize_url, handler.code_verifier
 
-def get_twitter_token(callback_url: str, current_url: str, twitter_verifier: str) -> dict: 
+def get_twitter_token(callback_url: str, current_url: str, twitter_verifier: str): 
     # then pass twitter_verifier back in here 
     handler = _oauth2_handler(callback_url, twitter_verifier)
 
@@ -56,3 +57,5 @@ def get_twitter_token(callback_url: str, current_url: str, twitter_verifier: str
         return handler.fetch_token(current_url)
     except OAuth2Error as e:
         raise Exception(e.description) from e
+    
+    
